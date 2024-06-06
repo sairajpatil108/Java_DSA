aa373dcaa511ffd68ca7827b6858cb691a1f5b35
Sairaj Patil <112862628+sairajpatil108@users.noreply.github.com>
2024-06-06 21:51:07 +0530

    hashing practice

--- dsa/hashing.java
+++ dsa/hashing.java
@@ -1,5 +1,28 @@
+import java.util.HashMap;
+// import java.util.Map;
+
 public class hashing {
-    
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> m = new HashMap<String, Integer>();
+        HashMap<String, Integer> n = new HashMap<String, Integer>(m);
+
+        m.put("sairaj", 1);
+        m.put("patil", 2);
+        m.put("PCCOE", 3);
+
+        
+        // Methods---
+
+        // System.out.println(m.get("sairaj"));
+        // System.out.println(m);
+        // m.clear();
+
+        System.out.println(m.entrySet());
+           
+        // System.out.println(m);
+        // System.out.println(m.values());
+    }
 }
 
 // int[] arr = { 1, 2, 2, 4, 5, 6, 6, 8, 9 };
@@ -14,21 +37,34 @@ public class hashing {
 // System.out.println(hash[i]);
 // }
 
-
-
 // public static void main(String[] args) {
-//     int hash[] = new int[26]; 
-//     String s = "sairaj";
-//     char arr[] = s.toCharArray(); 
-//     System.out.println(arr[1]);
+// int hash[] = new int[26];
+// String s = "sairaj";
+// char arr[] = s.toCharArray();
+// System.out.println(arr[1]);
 
-//     for (int i = 0; i < arr.length; i++) {
-//         if (arr[i] >= 'a' && arr[i] <= 'z') {
-//             hash[arr[i] - 'a']++;
-//         }
-//     }
+// for (int i = 0; i < arr.length; i++) {
+// if (arr[i] >= 'a' && arr[i] <= 'z') {
+// hash[arr[i] - 'a']++;
+// }
+// }
 
-//     for (int i = 0; i < hash.length; i++) {
-//         System.out.println((char) (i + 'a') + ": " + hash[i]);
-//     }
+// for (int i = 0; i < hash.length; i++) {
+// System.out.println((char) (i + 'a') + ": " + hash[i]);
+// }
+// }
+
+// int hash[] = new int[26];
+// String s = "sairaj";
+// char arr[] = s.toCharArray();
+// System.out.println(arr[1]);
+
+// for (int i = 0; i < arr.length; i++) {
+// if (arr[i] >= 'a' && arr[i] <= 'z') {
+// hash[arr[i] - 'a']++;
+// }
+// }
+
+// for (int i = 0; i < hash.length; i++) {
+// System.out.println((char) (i + 'a') + ": " + hash[i]);
 // }
